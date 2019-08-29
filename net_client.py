12851211bea74d3529b631489d8dc89e6b0bad99
egger0a6 <egger0a6@github.com>
2019-08-29 10:43:54 -0700

    add net_client file

---
+++ net_client.py
@@ -0,0 +1,15 @@
+import socket
+
+def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    addr = ("localhost", 34720)
+    sock.connect(addr)
+    
+    for i in range(100):
+        msg = str(i)
+        sock.sendall(msg.encode())
+        msg = sock.recv(1024).decode()
+        print(msg)
+
+
+main()
